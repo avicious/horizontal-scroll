e2e5c3d2a1a90cc73aca1fd79d610fa1d8aeaf05
Shubham Kumar <shubham.kr2951@gmail.com>
2026-02-27 22:33:25 +0530

    app complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,56 @@
+import { useRef } from "react";
+import { SAMPLE_DATA } from "./utils/data";
+
+const ITEM_WIDTH = 200;
+
 const App = () => {
+  const containerRef = useRef(null);
+
+  const handleScroll = (direction) => {
+    if (containerRef.current) {
+      const currentScroll = containerRef.current.scrollLeft;
+      const scrollAmount = direction === "left" ? -ITEM_WIDTH : ITEM_WIDTH;
+
+      containerRef.current.scrollTo({
+        left: currentScroll + scrollAmount,
+        behavior: "smooth",
+      });
+    }
+  };
+
   return (
-    <div>App</div>
+    <div className="container">
+      <div ref={containerRef} className="scroll">
+        <div className="content-box">
+          {SAMPLE_DATA.map((item) => (
+            <div
+              className="card"
+              key={item.id}
+              style={{ backgroundColor: item.color }}
+            >
+              <p>{item.id}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="action-btns">
+        <button
+          onClick={() => {
+            handleScroll("left");
+          }}
+        >
+          Scroll Left
+        </button>
+        <button
+          onClick={() => {
+            handleScroll("right");
+          }}
+        >
+          Scroll Right
+        </button>
+      </div>
+    </div>
   );
 };
 
